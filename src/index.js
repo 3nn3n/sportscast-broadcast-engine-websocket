cabd7efb21e0347c671218e5d610a50dd9e46d61
Nitesh Negi <nitesh_negi@outlook.com>
2026-04-08 17:36:25 +0530

    database schema added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
@@ -11,3 +12,4 @@ app.get('/', (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
