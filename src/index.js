630a12b792aef77ea6646990404cea6f1d819eaf
Nitesh Negi <nitesh_negi@outlook.com>
2026-04-08 19:21:48 +0530

    Post and Get route added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { matchRouter } from './route/matches.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -8,6 +9,8 @@ app.use(express.json());
 app.get('/', (req, res) => {
   res.send('Welcome to SportsCast API!');
 });
+
+app.use('/matches', matchRouter);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
